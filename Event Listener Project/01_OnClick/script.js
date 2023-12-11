c893c7f5b6c9603ecb8a3279a294d9585dc2340a
himamshupahal <himanshupahal503@gmail.com>
2023-12-11 20:34:36 +0530

    Excellent

---
+++ Event Listener Project/01_OnClick/script.js
@@ -0,0 +1,8 @@
+const boxElement = document.getElementById("box")
+
+// Add a Click Event Listerner To The Element //
+
+boxElement.addEventListener("click", function () {
+    alert(" You just click me 👍")
+}
+)
