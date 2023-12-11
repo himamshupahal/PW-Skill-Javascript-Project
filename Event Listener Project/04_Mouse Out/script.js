0e840d623008831252c91b788bc40f0117029308
himamshupahal <himanshupahal503@gmail.com>
2023-12-11 20:36:30 +0530

    Excellent

---
+++ Event Listener Project/04_Mouse Out/script.js
@@ -0,0 +1,6 @@
+const boxElement = document.getElementById("box");
+
+boxElement.addEventListener("mouseout" , function () {
+ alert ( " You left The Safe Zone 🙅 ( Mouse out do work properlty )" )
+}
+)
