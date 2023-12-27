fcdc9dbe5611243f5bb128064b50413727ff1579
himamshupahal <himanshupahal503@gmail.com>
2023-12-27 16:40:17 +0530

    Excellent

---
+++ MileStone Exam - 1/18. Change Text on Button Click/script.js
@@ -0,0 +1,10 @@
+function toggleHeading() {
+  
+    const heading = document.getElementById("heading");
+    if(heading.textContent === "The Most Affordable Learning Platform" ) {
+        heading.textContent = " PW SKILLS "
+    }
+    else{
+        heading.textContent = "The Most Affordable Learning Platform" ;
+    }
+};
