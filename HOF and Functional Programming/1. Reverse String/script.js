be21134a3e9281c768fa2fd1be07eaea71b88af8
himamshupahal <himanshupahal503@gmail.com>
2024-03-19 16:59:22 +0530

    Excellent

---
+++ HOF and Functional Programming/1. Reverse String/script.js
@@ -0,0 +1,13 @@
+function reverseString(st) {
+  let ans ="";
+  for(let i = st.length - 1 ; i >= 0;i--) {
+    ans += st[i];
+  }
+  return ans ;
+};
+
+let input = " PW Skills ";
+setTimeout(() => {
+  console.log(reverseString(input));
+},2000);
+
